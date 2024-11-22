3fab6074707c2aade93d1d5a9c8cebe0810a23c5
Sean Calkins <calkins.sean@gmail.com>
2024-11-21 21:12:49 -0700

    Add MapView, display markers with icons

--- mobile/app/src/main/java/com/example/codingchallenge/domain/model/Location.kt
+++ mobile/app/src/main/java/com/example/codingchallenge/domain/model/Location.kt
@@ -1,8 +1,44 @@
 package com.example.codingchallenge.domain.model
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Log
+import com.example.codingchallenge.R
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+
 data class Location(
     val attributes: List<Attribute>,
     val id: Int,
     val latitude: Double,
     val longitude: Double
-)
+) {
+    private val position: GeoPoint
+        get() = GeoPoint(latitude, longitude)
+
+    private val locationType: String
+        get() = attributes.firstOrNull { it.type == "location_type" }?.value ?: ""
+
+    fun toMarker(context: Context, mapView: MapView): Marker {
+        val marker = Marker(mapView)
+        marker.position = position
+        marker.icon = icon(context)
+        return marker
+    }
+
+    private fun icon(context: Context): Drawable? {
+        return when (locationType) {
+            "restaurant" -> context.getDrawable(R.drawable.restaurant_icon)
+            "bar" -> context.getDrawable(R.drawable.bar_icon)
+            "cafe" -> context.getDrawable(R.drawable.cafe_icon)
+            "museum" -> context.getDrawable(R.drawable.museum_icon)
+            "landmark" -> context.getDrawable(R.drawable.landmark_icon)
+            "park" -> context.getDrawable(R.drawable.park_icon)
+            else -> {
+                Log.d("OSMAPVIEW", "unknown location type: $locationType")
+                context.getDrawable(R.drawable.baseline_location_on_24)
+            }
+        }
+    }
+}
--- mobile/app/src/main/java/com/example/codingchallenge/presentation/mapscreen/MapScreen.kt
+++ mobile/app/src/main/java/com/example/codingchallenge/presentation/mapscreen/MapScreen.kt
@@ -3,13 +3,16 @@ package com.example.codingchallenge.presentation.mapscreen
 import androidx.activity.result.launch
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleStartEffect
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.example.codingchallenge.domain.model.Location
 import com.example.codingchallenge.presentation.mapscreen.composables.OSMMapView
 import com.example.codingchallenge.ui.theme.CodingChallengeTheme
 
@@ -20,7 +23,7 @@ fun NavGraphBuilder.mapScreen() {
         route = MapScreenRoute
     ) { backStackEntry ->
         val viewModel: MapScreenViewModel = hiltViewModel()
-
+        val model by viewModel.observableModel.collectAsStateWithLifecycle()
         LifecycleStartEffect(
             key1 = viewModel,
             lifecycleOwner = LocalLifecycleOwner.current,
@@ -30,25 +33,26 @@ fun NavGraphBuilder.mapScreen() {
             }
         )
 
-        MapScreen(modifier = Modifier.fillMaxSize())
+        MapScreen(modifier = Modifier.fillMaxSize(), model = model)
 
     }
 }
 
 @Composable
-private fun MapScreen(modifier: Modifier = Modifier) {
-    MapScreenContent(modifier = modifier)
+private fun MapScreen(modifier: Modifier = Modifier, model: MapScreenModel) {
+    MapScreenContent(modifier = modifier, model = model)
 }
 
 @Composable
-private fun MapScreenContent(modifier: Modifier = Modifier) {
-    OSMMapView(modifier = modifier)
+private fun MapScreenContent(modifier: Modifier = Modifier, model: MapScreenModel) {
+    OSMMapView(modifier = modifier, locations = model.locations)
 }
 
 @Preview
 @Composable
 private fun MapScreenContentPreview() {
     CodingChallengeTheme {
-        MapScreenContent(modifier = Modifier.fillMaxSize())
+        val model = MapScreenModel()
+        MapScreenContent(modifier = Modifier.fillMaxSize(), model = model)
     }
 }
